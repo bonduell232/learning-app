ef2a0b413e15d1404cdf1daff7cc4e8a103fd1c8
Marcus <bonduell232@gmail.com>
2026-03-15 21:53:42 +0100

    security: add HTTP strict headers (OWASP A05)

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,35 @@ const nextConfig: NextConfig = {
     },
   },
   serverExternalPackages: ['@google/genai'],
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          }
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
